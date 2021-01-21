a2883348fdb41663b8b85dc60e68ba891a99a206
alfiiasharipova <alfiiasharipova@gmail.com>
2021-01-21 22:25:29 +0300

    Tests fixed

--- src/test/java/ru/netology/WebInterfaceTest.java
+++ src/test/java/ru/netology/WebInterfaceTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.cssClass;
@@ -10,64 +11,66 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class WebInterfaceTest {
 
-    @Test
-    void shouldSubmitRequest()
-    {
+    @BeforeEach
+    void serUp() {
         open("http://localhost:9999");
-        SelenideElement form=$(".form");
-        form.$("[name=name]").setValue("Василий");
+    }
+
+    @Test
+    void shouldSubmitRequest() {
+        SelenideElement form = $(".form");
+        form.$("[name=name]").setValue("Кашин Василий");
         form.$("[name=phone]").setValue("+79270000000");
         form.$(".checkbox__box").click();
         form.$(".button").click();
-        $(".paragraph").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(".paragraph")
+                .shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void shouldNotSubmitRequestWithInvalidName()
-    {
-        open("http://localhost:9999");
-        SelenideElement form=$(".form");
+    void shouldNotSubmitRequestWithInvalidName() {
+        SelenideElement form = $(".form");
         form.$("[name=name]").setValue("Vasiliy");
+        form.$("[name=phone]").setValue("+79270000000");
+        form.$(".checkbox__box").click();
         form.$(".button").click();
-        $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
+        $("[data-test-id=name] .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    void shouldNotSubmitRequestWithEmptyName()
-    {
-        open("http://localhost:9999");
-        SelenideElement form=$(".form");
+    void shouldNotSubmitRequestWithEmptyName() {
+        SelenideElement form = $(".form");
+        form.$("[name=phone]").setValue("+79270000000");
+        form.$(".checkbox__box").click();
         form.$(".button").click();
-        $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldNotSubmitRequestWithInvalidPhone()
-    {
-        open("http://localhost:9999");
-        SelenideElement form=$(".form");
-        form.$("[name=name]").setValue("Василий");
+    void shouldNotSubmitRequestWithInvalidPhone() {
+        SelenideElement form = $(".form");
+        form.$("[name=name]").setValue("Кашин Василий");
         form.$("[name=phone]").setValue("+79270");
+        form.$(".checkbox__box").click();
         form.$(".button").click();
-        $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
+        $("[data-test-id=phone] .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
-    void shouldNotSubmitRequestWithEmptyPhone()
-    {
-        open("http://localhost:9999");
-        SelenideElement form=$(".form");
-        form.$("[name=name]").setValue("Василий");
+    void shouldNotSubmitRequestWithEmptyPhone() {
+        SelenideElement form = $(".form");
+        form.$("[name=name]").setValue("Кашин Василий");
+        form.$(".checkbox__box").click();
         form.$(".button").click();
-        $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldNotSubmitRequestWithoutCheckbox()
-    {
-        open("http://localhost:9999");
-        SelenideElement form=$(".form");
-        form.$("[name=name]").setValue("Василий");
+    void shouldNotSubmitRequestWithoutCheckbox() {
+        SelenideElement form = $(".form");
+        form.$("[name=name]").setValue("Кашин Василий");
         form.$("[name=phone]").setValue("+79270000000");
         form.$(".button").click();
         $(".checkbox").shouldHave(cssClass("input_invalid"));
